2bc810a6d70ec00ea10132f1c5b17dee3184f9e7
wzono <wzono.me@gmail.com>
2023-06-08 22:50:58 +0800

    refactor: format and add basic page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,19 @@
+export default function Home () {
+  return (
+    <div
+      className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8 bg-gray-100
+      "
+    >
+      <p className="text-sky-500 text-3xl">
+        Hello Index Page
+      </p>
+    </div>
+  );
+}
