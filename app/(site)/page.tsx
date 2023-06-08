047378b39978df83d9e6c2ec6d18b6fb3b8f4a07
wzono <wzono.me@gmail.com>
2023-06-08 22:51:13 +0800

    test: test verified

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,11 +8,12 @@ export default function Home () {
         justify-center
         py-12
         sm:px-6
-        lg:px-8 bg-gray-100
+        lg:px-8
+        bg-gray-100
       "
     >
       <p className="text-sky-500 text-3xl">
-        Hello Index Page
+        Index Page
       </p>
     </div>
   );
