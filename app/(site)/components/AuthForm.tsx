57846a866e6b91894df159384699c3d6570c3854
wzono <wzono.me@gmail.com>
2023-06-11 23:20:46 +0800

    feat: add social buttons

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -3,6 +3,8 @@
 import { useCallback, useState } from 'react'
 import type { FieldValues, SubmitHandler } from 'react-hook-form'
 import { useForm } from 'react-hook-form'
+import { BsGithub, BsGoogle } from 'react-icons/bs'
+import AuthSocialButton from './AuthSocialButton'
 import Button from '@/app/components/Button'
 import Input from '@/app/components/inputs/Input'
 
@@ -85,8 +87,48 @@ const AuthForm = () => {
               {variant === 'LOGIN' ? 'Login' : 'Register'}
             </Button>
           </div>
+
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="absolute inset-0 flex items-center"
+            >
+              <div
+                className="
+                  w-full
+                  border-t
+                  border-gray-300
+                "
+              />
+            </div>
+            <div
+              className="relative flex justify-center text-sm"
+            >
+              <span className="px-2 bg-white text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-4">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('Github')}
+            >
+              Github
+            </AuthSocialButton>
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('Google')}
+            >
+              Google
+            </AuthSocialButton>
+          </div>
+        </div>
       </div>
+
     </div>
   )
 }
