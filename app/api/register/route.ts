fcdbaa9831b614b20c16077f4e21762a076107a7
wzono <wzono.me@gmail.com>
2023-06-13 22:27:21 +0800

    feat: add basic register api

---
+++ app/api/register/route.ts
@@ -0,0 +1,32 @@
+import bcrypt from 'bcrypt'
+import { NextResponse } from 'next/server'
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { email, password, name } = body
+
+    if (!email || !password || !name) {
+      return NextResponse.json({ error: 'Missing email, password or name' },
+        {
+          status: 400,
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+        name,
+      },
+    })
+
+    return NextResponse.json(user)
+  }
+  catch (error: any) {
+    console.error('register user error', error)
+    return NextResponse.json({ error })
+  }
+}
