0d70cf8a4565448f360bd0b1706f95011fd3d747
wzono <wzono.me@gmail.com>
2023-06-19 00:57:35 +0800

    fix: get right responsive style

--- app/(site)/users/page.tsx
+++ app/(site)/users/page.tsx
@@ -3,7 +3,7 @@ import EmptyState from './components/EmptyState'
 const Users = () => {
   return (
     <div
-      className="hidden md:block md:pl-80 h-full"
+      className="hidden lg:block lg:pl-80 h-full"
     >
       <EmptyState />
     </div>
